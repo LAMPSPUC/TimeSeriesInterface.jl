d736396d3f9039c3fcf7a85c4bb4fe0361c95cb0
IagoChavarry <48952504+iagochavarry@users.noreply.github.com>
2020-08-21 21:51:59 -0300

    fix bug in mean_probabilistic_calibration (#17)
    * fix bug in mean_probabilistic_calibration
    
    * fix interval width bug
    
    * change order of for's
    
    Co-authored-by: Iago Chavarry <iagochavarry@MacBook-Pro-de-Iago.local>

--- src/forecast.jl
+++ src/forecast.jl
@@ -250,8 +250,8 @@ function mean_probabilistic_calibration(forecast::Vector{ScenariosForecastMetric
                                                 undef, 
                                                 length(forecast[1].probabilistic_calibration)
                                                 )
-    for forec in forecast
-        for t in 1:length(m_probabilistic_calibrations)
+    for t in 1:length(m_probabilistic_calibrations)
+        for forec in forecast
             m_probabilistic_calibrations[t] = Dict{Float64, Float64}()
             for (k, v) in forec.probabilistic_calibration[t]
                 if !haskey(m_probabilistic_calibrations[t], k)
@@ -270,8 +270,8 @@ function mean_interval_width(forecast::Vector{ScenariosForecastMetrics})
                                                 undef, 
                                                 length(forecast[1].interval_width)
                                                 )
-    for forec in forecast
-        for t in 1:length(m_interval_width)
+    for t in 1:length(m_interval_width)
+        for forec in forecast
             m_interval_width[t] = Dict{Float64, Float64}()
             for (k, v) in forec.interval_width[t]
                 if !haskey(m_interval_width[t], k)
